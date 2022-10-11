56ef5814025b482edfb2e71015c13fa2a812f22d
Engiineeer <ntpzlwo@gmail.com>
2022-10-11 17:44:04 +0300

    sub

---
+++ restaurants/models.py
@@ -0,0 +1,14 @@
+from operator import mod
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+   # create_at = models.DateTimeField()
